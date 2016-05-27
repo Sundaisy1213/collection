adeec9807b9b658dd4a00d76aa33802a31b41c08
Sundaisy1213 <daisy1213@163.com>
2016-05-27 16:13:49 +0800

    last

---
+++ src/collect_all_even.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function collect_all_even(collection) {
+	var nums = [];
+
+ 	collection.forEach(function(ele){
+ 		if(ele % 2 == 0){
+ 			nums.push(ele);
+ 		}
+ 	});
+
+ 	return nums;
+}
+module.exports = collect_all_even;
